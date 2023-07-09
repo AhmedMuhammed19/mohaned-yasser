ba0bbe4d2d29b13e709307042c3941091cb3da35
ahmed <ahmedmobuss@gmail.com>
2023-07-10 00:23:56 +0300

    new

--- js/script.js
+++ js/script.js
@@ -54,3 +54,67 @@ const writeLoop = async () => {
 };
 
 writeLoop();
+
+let sectionsScroll = document.querySelectorAll("section");
+let navLinksScroll = document.querySelectorAll(".landing nav a");
+
+document.addEventListener('scroll', () =>{
+  const header =document.querySelector('header');
+
+if (window.scrollY>0){
+  header.classList.add('scrolled')
+}
+else{
+  header.classList.remove('scrolled')
+}
+})
+
+
+var sections = document.querySelectorAll("section");
+
+onscroll = function () {
+  var scrollPosition = document.documentElement.scrollTop;
+
+  sections.forEach((section) => {
+    if (
+      scrollPosition >= section.offsetTop - section.offsetHeight * 0.25 &&
+      scrollPosition <
+        section.offsetTop + section.offsetHeight - section.offsetHeight * 0.25
+    ) {
+      var currentId = section.attributes.id.value;
+      removeAllActiveClasses();
+      addActiveClass(currentId);
+    }
+  });
+};
+
+var removeAllActiveClasses = function () {
+  document.querySelectorAll("nav a").forEach((el) => {
+    el.classList.remove("active");
+  });
+};
+
+var addActiveClass = function (id) {
+  // console.log(id);
+  var selector = `nav a[href="#${id}"]`;
+  document.querySelector(selector).classList.add("active");
+};
+
+var navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    var currentId = e.target.attributes.href.value;
+    var section = document.querySelector(currentId);
+    var sectionPos = section.offsetTop;
+    // section.scrollIntoView({
+    //   behavior: "smooth",
+    // });
+
+    window.scroll({
+      top: sectionPos,
+      behavior: "smooth",
+    });
+  });
+});
